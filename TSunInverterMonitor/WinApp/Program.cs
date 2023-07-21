55da345640150aa89759defb6ddd0a494d130d09
Marcus Tastler <mtastler@t-online.de>
2023-07-21 13:38:31 +0200

    Projektdatei korrigiert

--- TSunInverterMonitor/WinApp/Program.cs
+++ TSunInverterMonitor/WinApp/Program.cs
@@ -1,18 +1,15 @@
+using System.Windows.Forms;
 using System.Runtime.CompilerServices;
 
 namespace NZZ.TSIM.WinApp
 {
   internal static class Program
   {
-    /// <summary>
-    ///  The main entry point for the application.
-    /// </summary>
     [STAThread]
     static void Main()
     {
-      // To customize application configuration such as set high DPI settings or default font,
-      // see https://aka.ms/applicationconfiguration.
-      ApplicationConfiguration.Initialize();
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
       Application.Run(new FormMain());
     }
 
